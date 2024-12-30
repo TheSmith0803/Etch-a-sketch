d87290e3f8f3b0d4db684235888efecedd95012f
TheSmith0803 <ethan.m.smith0803@gmail.com>
2024-12-30 14:43:06 -0500

    trying to add divs to container

---
+++ app.js
@@ -0,0 +1,14 @@
+//canvas for etch a sketch
+const c  = document.querySelector("#container");
+//container to hold different option buttons for functionality
+const opt = document.querySelector("#options");
+
+for (let i = 0; i < 16; i++) {
+
+    const div = document.createElement("div");
+    div.setAttribute("class", "canvas-child");
+    c.appendChild(div);
+
+}
+
+const c_child = document.querySelectorAll(".canvas-child");
