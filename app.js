3ff57fbf8e65eb4a99698eff3503666fed7ec6ed
TheSmith0803 <ethan.m.smith0803@gmail.com>
2024-12-30 21:08:08 -0500

    not sure how to check if integer properly

--- app.js
+++ app.js
@@ -10,9 +10,21 @@ const sizeBtn = document.querySelector("#size-button");
 const colorBtn = document.querySelector("#color-button");
 const clearBtn = document.querySelector("#clear");
 
+//EVENT LISTENERS FOR BUTTONS
+
+//hover event
+
 //change size of grid here
 sizeBtn.addEventListener("click", () => {
-
+    let size = prompt("Please input a number from 1 to 99");
+    if (size < 1 || size > 99) {
+        alert("Please only enter a number between 1 and 99");
+    }
+    else if (Number.isInteger(size)) {
+        alert("Please no characters or decimal numbers!");
+    } else {
+        makeGrid(size, size);
+    }
 });
 
 //change color of brush
@@ -20,12 +32,15 @@ colorBtn.addEventListener("click", () => {
 
 });
 
+//FUNCTIONS FOR FUNCTIONALITY
+
 let resizeGrid = () => {
-    
+
 };
 
 // function to make the grid
 let makeGrid = (row, col) => {
+    c.innerHTML = ''; //clear the divs in the etch-a-sketch container
     for (i = 0; i < row; i++) {
         for (j = 0; j < col; j++) {
             const size = 1000 / row; //constant value to represent pixel size of container
@@ -40,12 +55,3 @@ let makeGrid = (row, col) => {
 };
 
 makeGrid(16, 16);
-
-
-
-
-
-//function to clear the grid
-const clearGrid = () => {
-    c.removeChild("");
-};
