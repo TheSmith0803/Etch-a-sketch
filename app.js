b120e584c9ef83cbf6f69c9ff41f2e850bc0d7aa
TheSmith0803 <ethan.m.smith0803@gmail.com>
2024-12-31 19:27:01 -0500

    changed grid-item to class

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const c  = document.querySelector("#container");
 const opt = document.querySelector("#options");
 
 const body = document.querySelector("body");
-const grid = document.querySelectorAll("#grid-item");
+const grid = document.querySelectorAll(".grid-item");
+let grid_arr = Array.from(grid);
 
 //buttons
 const sizeBtn = document.querySelector("#size-button");
@@ -27,6 +28,8 @@ sizeBtn.addEventListener("click", () => {
     }
 });
 
+
+
 //change color of brush
 colorBtn.addEventListener("click", () => {
 
@@ -45,7 +48,7 @@ let makeGrid = (row, col) => {
         for (j = 0; j < col; j++) {
             const size = 1000 / row; //constant value to represent pixel size of container
             const div = document.createElement("div");
-            div.setAttribute("id", "grid-item");
+            div.setAttribute("class", "grid-item");
             div.style.width = size + "px";
             div.style.height = size + "px";
 
